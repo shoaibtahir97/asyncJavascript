4e0a06508f7bd2828b2ffa084f2d4b7bee4252da
shoaib97 <49276225+shoaibtahir97@users.noreply.github.com>
2020-08-11 20:00:18 +0500

    setTimeOut function added
    setTimeOut function will delay the function and it will be printed in the end

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 console.log ("Before preparing food.");
 
 function prepareFood(){
-    console.log("Food is being prepared");
+    setTimeout(()=>{
+        console.log("Food is being prepared");
+    });
 }
 
 prepareFood();
+
 console.log("The food is Prepared");
