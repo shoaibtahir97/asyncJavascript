657e50b48e99b92397c665f5f84c29f7e79f5832
shoaib97 <49276225+shoaibtahir97@users.noreply.github.com>
2020-08-14 18:40:47 +0500

    async js using multiple callbacks

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 console.log ("Before preparing food.");
 
-function prepareFood(callback){
+function prepareSouffle(callback){
     setTimeout(()=>{
-        console.log("Food is being prepared");
-        callback("Food is ready")
+        console.log("Souffle is being prepared");
+        callback("Souffle is ready")
     },2000);
 }
 
-function myCallBack(value){
+function prepareFrenchToast(callback){
+    setTimeout(()=>{
+        console.log("Preparing French Toast");
+        callback("French Toast is ready");
+    },4000);
+}
+
+function firstCallBack(value){
     console.log("Your",value);
+    prepareFrenchToast(secondCallBack);//prepareFrenchToast fn will wait for the first callback to 
+                                       //complete and once it is done it will call the secondcallback
 } 
 
-prepareFood(myCallBack);
+function secondCallBack(value){
+    console.log("Sir your ",value);
+}
+
+prepareSouffle(firstCallBack);
+
+
 
 console.log("The food is Prepared");
